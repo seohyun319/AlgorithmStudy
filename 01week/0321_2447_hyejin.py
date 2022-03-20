5b05614bc7bf9044a25cc826c8dd97ccb62c1c83
rimbaud2319 <81897536+rimbaud2319@users.noreply.github.com>
2022-03-20 16:58:32 +0900

    Add files via upload

---
+++ 01week/0321_2447_hyejin.py
@@ -0,0 +1,19 @@
+def append_star(n):
+    if n==1:
+        return ['*']
+    
+    stars = append_star(n//3)
+    list_star = []
+
+    for i in stars:
+        list_star.append(i * 3)
+    for i in stars:
+        list_star.append(i + ' ' * (n//3) + i)
+    for i in stars:
+        list_star.append(i * 3)
+
+    return list_star
+
+N = int(input())
+print('\n'.join(append_star(N)))
+
