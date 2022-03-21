644cc96a13faa2cd7831959c7a0bd4ea0174d773
seohyun319 <tjguselly0319@gmail.com>
2022-03-21 14:59:57 +0900

    김서현 별 찍기

---
+++ 01week/0321_2447_seohyun.py
@@ -0,0 +1,31 @@
+# silver1 - 2447. 별 찍기 - 10
+
+import sys
+put = sys.stdin.readline
+
+# 재귀로 별 찍기
+
+n: int = int(put())
+
+# 가운데 뚫림. 
+# 3^1일 땐 1 인덱스, 3으로 나눈 나머지 1
+# 3^2일 땐 3 4 5 인덱스, 3으로 나눈 몫 1
+
+def star(n: int, array: list) -> list:
+    lists: list = []
+    if n == 3:
+        return array
+    else: 
+        for i in array:            
+            array.append(i*3)
+            array.append(i*3)
+            array.append(i*3)
+            # array.append(i)
+            # array.append('***')
+        return star(n//3)
+            
+
+    # return star(n/3) + star(n/3)
+
+star_list = ["***", "* *", "***"]
+print("\n".join(star(n)))
