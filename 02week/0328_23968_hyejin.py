80b58ad1619c656acf4589539a791244d3b96a05
rimbaud2319 <81897536+rimbaud2319@users.noreply.github.com>
2022-03-27 23:47:41 +0900

    송혜진 버블정렬1

---
+++ 02week/0328_23968_hyejin.py
@@ -0,0 +1,20 @@
+﻿def bubble_selection(n, k):
+    count = 0
+    l = len(n)
+    for i in range(l-1):
+        for j in range(l-1-i):
+            if n[j] > n[j+1]:
+                count += 1
+                if count == k:
+                    print(n[j+1], n[j])
+                n[j], n[j+1] = n[j+1], n[j]
+    if count < k:
+        print(-1)
+
+n, k = input().split()
+n = int(n)
+k = int(k)
+array = list(map(int, input().split()))
+bubble_selection(array, k)
+
+#시간 초과
