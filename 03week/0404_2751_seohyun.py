95e0a8e635b475e4867751271632e5361a6de9fa
seohyun319 <tjguselly0319@gmail.com>
2022-04-04 02:26:25 +0900

    김서현 수 정렬하기 2

---
+++ 03week/0404_2751_seohyun.py
@@ -0,0 +1,30 @@
+# silver2. 수 정렬하기 2
+# 시간 초과
+
+import sys
+put = sys.stdin.readline
+
+n: int = int(put())
+array: list = [int(put()) for _ in range(n)]
+
+
+def merge_sort(array: list) -> list:
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left_array = merge_sort(array[mid:])
+    right_array = merge_sort(array[:mid])
+
+    result: list = []
+    while left_array and right_array:
+        if left_array[0] < right_array[0]:
+            result.append(left_array.pop(0))
+        else:
+            result.append(right_array.pop(0))
+    result = result + left_array + right_array
+
+    return result
+
+
+print(*merge_sort(array), sep='\n')
