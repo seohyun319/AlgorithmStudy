34dfd23318bec85e7455e245225d0f99a5cb544b
gyeongminK <66426083+gyeongminK@users.noreply.github.com>
2022-04-04 15:44:58 +0900

    Update 0404_2751_gyeongmin.py

--- 03week/0404_2751_gyeongmin.py
+++ 03week/0404_2751_gyeongmin.py
@@ -1,33 +1,63 @@
 #2751
-n = int(input())
-array = list()
-for i in range(n):
-    array.append(int(input()))
-print("초기", array)
+# n = int(input())
+# array = list()
+# for i in range(n):
+#     array.append(int(input()))
+# print("초기", array)
+
+# def merge_sort(array):
+
+#     if len(array)<=1:
+#         return
+
+#     mid = len(array)//2
+
+#     a1 = array[:mid]
+#     a2 = array[mid:]
+
+#     merge_sort(a1)
+#     merge_sort(a2)
+
+#     result = list()
+#     while a1 and a2:
+#         if a1[0] < a2[0]:
+#             result.append(a1.pop(0))
+#         else:
+#             result.append(a2.pop(0))
+
+#     result = result + a1 + a2
+#     print(result)
+#     return result
+
+# # d = [1,3,5,2,4,6]
+# print('최종결과', merge_sort(array))
+
+
 
 def merge_sort(array):
 
     if len(array)<=1:
-        return
+        return array
 
     mid = len(array)//2
 
-    a1 = array[:mid]
-    a2 = array[mid:]
+    a1 = merge_sort(array[:mid])
+    a2 = merge_sort(array[mid:])
 
-    merge_sort(a1)
-    merge_sort(a2)
-
-    result = list()
+    result = []
     while a1 and a2:
+        print('a1', a1)
+        print('a2', a2)
         if a1[0] < a2[0]:
+            print(a1[0])
             result.append(a1.pop(0))
         else:
+            print(a2[0])
             result.append(a2.pop(0))
 
     result = result + a1 + a2
-    print(result)
+    #print('result',result)
     return result
 
 # d = [1,3,5,2,4,6]
-print('최종결과', merge_sort(array))
+print(merge_sort(array))
