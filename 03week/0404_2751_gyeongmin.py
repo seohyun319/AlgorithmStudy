2f6d06d29f5b6e011411e55166f62d94eae85e10
gyeongminK <66426083+gyeongminK@users.noreply.github.com>
2022-04-04 14:38:56 +0900

    김경민 수 정렬하기2

---
+++ 03week/0404_2751_gyeongmin.py
@@ -0,0 +1,33 @@
+#2751
+n = int(input())
+array = list()
+for i in range(n):
+    array.append(int(input()))
+print("초기", array)
+
+def merge_sort(array):
+
+    if len(array)<=1:
+        return
+
+    mid = len(array)//2
+
+    a1 = array[:mid]
+    a2 = array[mid:]
+
+    merge_sort(a1)
+    merge_sort(a2)
+
+    result = list()
+    while a1 and a2:
+        if a1[0] < a2[0]:
+            result.append(a1.pop(0))
+        else:
+            result.append(a2.pop(0))
+
+    result = result + a1 + a2
+    print(result)
+    return result
+
+# d = [1,3,5,2,4,6]
+print('최종결과', merge_sort(array))
