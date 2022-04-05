c9d12464465531bb27e0ff32c6e2f0e2aa213e6a
gyeongminK <66426083+gyeongminK@users.noreply.github.com>
2022-04-05 16:57:47 +0900

    김경민 수 정렬하기2

--- 03week/0404_2751_gyeongmin.py
+++ 03week/0404_2751_gyeongmin.py
@@ -1,38 +1,11 @@
 #2751
+
+# pop 사용해서 푸는 법
 n = int(input())
 array = list()
 for i in range(n):
     array.append(int(input()))
 print("초기", array)
-
-# def merge_sort(array):
-
-#     if len(array)<=1:
-#         return
-
-#     mid = len(array)//2
-
-#     a1 = array[:mid]
-#     a2 = array[mid:]
-
-#     merge_sort(a1)
-#     merge_sort(a2)
-
-#     result = list()
-#     while a1 and a2:
-#         if a1[0] < a2[0]:
-#             result.append(a1.pop(0))
-#         else:
-#             result.append(a2.pop(0))
-
-#     result = result + a1 + a2
-#     print(result)
-#     return result
-
-# # d = [1,3,5,2,4,6]
-# print('최종결과', merge_sort(array))
-
-
 
 def merge_sort(array):
 
@@ -59,5 +32,49 @@ def merge_sort(array):
     #print('result',result)
     return result
 
-# d = [1,3,5,2,4,6]
 print(merge_sort(array))
+
+
+
+# 시간초과 안 되게 푸는 법
+def merge_sort(array):
+
+    if len(array)<=1:
+        return array
+
+    mid = len(array)//2
+
+    a1 = merge_sort(array[:mid])
+    a2 = merge_sort(array[mid:])
+
+    result = []
+
+    i1, i2 = 0, 0
+
+    while len(a1) > i1 and len(a2) > i2 :
+        if a1[i1] < a2[i2]:
+            result.append(a1[i1])
+            i1+=1
+        else:
+            result.append(a2[i2])
+            i2+=1
+
+    while len(a1) > i1 and len(a2) <= i2 :
+        result.append(a1[i1])
+        i1+=1
+
+    while len(a2) > i2 and len(a1) <= i1 :
+        result.append(a2[i2])
+        i2 += 1
+
+    return result
+
+n = int(input())
+array = list()
+for i in range(n):
+    array.append(int(input()))
+
+sorted_array = merge_sort(array)
+
+for i in range(n):
+    print(sorted_array[i])
