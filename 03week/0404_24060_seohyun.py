e2b64ce7c58fcdfa975bd6c2df8682a255c45063
seohyun319 <tjguselly0319@gmail.com>
2022-04-17 16:06:16 +0900

    김서현 병합정렬 1 문제 예시랑 정렬 과정 다른 거 해결

--- 03week/0404_24060_seohyun.py
+++ 03week/0404_24060_seohyun.py
@@ -1,7 +1,8 @@
 # silver5. 알고리즘 수업 - 병합 정렬 1
-# 시간 초과
+# 문제 예시랑 정렬 과정 다른 거 해결
+# 문제 예시 순서는 2 / 3이 아니라 3 / 2로 나눔 => 올림
 
-import sys
+import sys, math
 put = sys.stdin.readline
 
 n, k = map(int, put().split())  # 배열의 크기, 저장 횟수
@@ -13,10 +14,9 @@ def merge_sort(array: list) -> list:
     if len(array) <= 1:
         return array
 
-    mid = len(array) // 2
+    mid = math.ceil(len(array) / 2) # 올림: 문제에서 요구하는 순서에 따름
     left_array = merge_sort(array[:mid])
     right_array = merge_sort(array[mid:])
-    # print("LR", left_array, right_array, end=' ')
 
     result: list = []
     while left_array and right_array:
@@ -28,29 +28,20 @@ def merge_sort(array: list) -> list:
             pop_num = right_array.pop(0)
             result.append(pop_num)
             save_num.append(pop_num)
-        # print("res", result, left_array, right_array, save_num)
-
-    # result = result + left_array + right_array
 
     while left_array:  # 왼쪽 배열이 남은 경우
         pop_num = left_array.pop(0)
         result.append(pop_num)
         save_num.append(pop_num)
-    # print("left", result, end=' ')
     while right_array:  # 오른쪽 배열이 남은 경우
         pop_num = right_array.pop(0)
         result.append(pop_num)
         save_num.append(pop_num)
-    # print("right", result)
 
     return result
 
 
-# print(merge_sort(array))
-
 merge_sort(array)
-# print(save_num)
-
 
 if len(save_num) >= k:
     # 인덱스 0부터 시작하니까 k번째 값 프린트하기 위해 1 빼줌
