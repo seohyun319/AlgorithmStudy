946d713a86d8ad8f1bf7ae895c0f8bed5b66f94f
seohyun319 <tjguselly0319@gmail.com>
2022-04-23 00:09:33 +0900

    김서현 버블정렬 1

--- 02week/0328_23968_seohyun.py
+++ 02week/0328_23968_seohyun.py
@@ -1,4 +1,7 @@
+# bronze 1. 알고리즘 수업 - 버블 정렬 1
+# pypy로 맞음
 import sys
+
 put = sys.stdin.readline
 
 n, k = map(int, put().split())  # 배열 A의 크기, 교환 횟수
@@ -7,12 +10,17 @@ cnt: int = 0  # 교환 횟수
 
 
 def bubble_sort(a):
+    global cnt
     n = len(a)
     for i in range(n):
         for j in range(n - i - 1):
             if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
+                cnt += 1
+                if cnt == k:
+                    print(a[j], a[j + 1])
 
 
 bubble_sort(array)
-print(array)
+if cnt < k:
+    print(-1)
