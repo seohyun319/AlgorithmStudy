797134afc5910a562e2c5813480a3d5b5f52a9ac
Doyoon99 <61955796+Doyoon99@users.noreply.github.com>
2022-05-08 13:22:51 +0900

    박도윤 암기왕

---
+++ 07week/0509_2776_doyoon.py
@@ -0,0 +1,15 @@
+import sys
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s1 = list(sys.stdin.readline().split())
+    m = int(input())
+    s2 = list(sys.stdin.readline().split())
+    for s in s2:
+        if s in s1:
+            print(1)
+        else:
+            print(0)
+           
+# 시간 초과?! 
