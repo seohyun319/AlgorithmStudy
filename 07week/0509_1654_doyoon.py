e25ce395a46bc376e9d4da79a365c80dc63abaae
Doyoon99 <61955796+Doyoon99@users.noreply.github.com>
2022-05-08 13:58:19 +0900

    박도윤 랜선 자르기

---
+++ 07week/0509_1654_doyoon.py
@@ -0,0 +1,17 @@
+import sys
+k, n = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(K)]
+start = 1
+end = max(lan)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    for i in lan:
+        total += i // mid
+        
+    if total >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
