891379f35ca4f1799155f3729f78125f04bca53f
Gyeongmin Kim <66426083+gyeongminK@users.noreply.github.com>
2022-05-16 13:52:31 +0900

    김경민 DFS와 BFS

--- 07week/0516_1260_gyeongmin.py
+++ 07week/0516_1260_gyeongmin.py
@@ -1,4 +1,6 @@
 # 1260 DFS와 BFS
+from collections import deque
+
 def dfs(graph, v, visited):
 	visited[v] = True
 	print(v, end=' ')
@@ -6,15 +8,32 @@ def dfs(graph, v, visited):
 		if not visited[i]:
 			dfs(graph, i, visited)
 
+def bfs(graph, v, visited):
+	queue = deque([v])
+	visited[v] = True
+
+	while queue:
+		num = queue.popleft()
+		print(num, end=' ')
+
+		for i in graph[num]:
+			if not visited[i]:
+				queue.append(i)
+				visited[i] = True
+
+
 n, m, v = map(int,input().split()) #정점, 간선, 시작점
 graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
-	v1, v2 = map(int, input().split())
-	graph[v1].append(v2)
-	graph[v2].append(v1)
-
+	x, y = map(int, input().split())
+	graph[x].append(y)
+	graph[y].append(x)
 #print(graph)
-visited = [False]*9
+
+
+visited = [False for _ in range(n + 1)]
 
 dfs(graph, v, visited)
+print()
+bfs(graph, v, visited)
