41391847054151958793f130df5522467264f431
rimbaud2319 <81897536+rimbaud2319@users.noreply.github.com>
2022-05-16 13:42:02 +0900

    송혜진 DFS와 BFS

--- 07week/0516_1260_hyejin.py
+++ 07week/0516_1260_hyejin.py
@@ -1,5 +1,8 @@
+import sys
+put = sys.stdin.readline
+
 def dfs_bfs():
-    n, m, v = map(int, input().split())
+    n, m, v = map(int, put().split())
     for i in range(m):
         start, last = map(int, input().split())
         line.append((start, last))
