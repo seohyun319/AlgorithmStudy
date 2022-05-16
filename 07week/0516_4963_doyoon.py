49476a0398dec8af7fc2c96a6c453f598c901369
Doyoon99 <61955796+Doyoon99@users.noreply.github.com>
2022-05-16 15:44:37 +0900

    박도윤 섬의 개수

--- 07week/0516_4963_doyoon.py
+++ 07week/0516_4963_doyoon.py
@@ -4,3 +4,16 @@ sys.setrecursionlimit(5000000)
 def dfs(y,x):
     if x < 0 or x >= w or y < 0 or y >= h:
         return False
+    
+    if graph[y][x] == 1:
+        graph[y][x] = 0
+        dfs(y,x-1)
+        dfs(y,x+1)
+        dfs(y-1,x)
+        dfs(y+1,x)
+        dfs(y-1,x-1)
+        dfs(y-1,x+1)
+        dfs(y+1,x-1)
+        dfs(y+1,x+1)
+        return True
+    return False
