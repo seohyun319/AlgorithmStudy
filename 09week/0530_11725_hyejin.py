4fb21b4a599a53dd1fe463f5e97a21c9463af667
rimbaud2319 <81897536+rimbaud2319@users.noreply.github.com>
2022-05-30 14:52:14 +0900

    송혜진 트리의 부모 찾기

--- 09week/0530_11725_hyejin.py
+++ 09week/0530_11725_hyejin.py
@@ -10,7 +10,24 @@ for i in range(n-1):
     connect[line[0]-1][line[1]-1] = 1
     connect[line[1]-1][line[0]-1] = 1
 
-for i in range(n):
-    #parent = 
-    print(parent)
+parent = []
+for i in range(1, n):
+    exist = []
+    #행렬을 확인해서
+    for j in range(n):
+        if connect[i][j] == 1:
+            exist.append(j+1)
+    print(exist)
+    #1이 있으면 parent는 1
+    if 1 in exist:
+        parent.append(1)
+    else:
+        #값이 하나면 parent는 해당 값
+        if len(exist) == 1:
+            parent.append(exist[0])
+        #값이 두개일 때
+        else:
+            parent.append(exist)
 
+for i in range(len(parent)):
+    print(parent[i])
